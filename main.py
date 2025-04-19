591bd527bbcd76da3ebfd554c44960ed3c87c531
Darren Brady <dcgbfinancial@gmail.com>
2025-04-19 06:32:43 +0000

    addition of API for the newsapi.org pulling the infomation for TESLA if the percentage increase/decrease exceeds 5%

--- main.py
+++ main.py
@@ -12,22 +12,31 @@ data = r.json()
 
 # Obtain the dates for the 1 day from now and 2 days from now.
 today = datetime.date.today()
-yesterday = str(today - datetime.timedelta(days=1))
-day_b4_yesterday = str(today - datetime.timedelta(days=2))
+yesterday = str('2025-04-17')
+day_b4_yesterday = str('2025-04-16')
+# yesterday = str(today - datetime.timedelta(days=1))
+# day_b4_yesterday = str(today - datetime.timedelta(days=2))
 
-yesterdays_close = float(data['Time Series (Daily)'][yesterday]['4. close'])
+# yesterdays_close = float(data['Time Series (Daily)'][yesterday]['4. close'])
+yesterdays_close = 100
 day_b4_yesterday_close = float(data['Time Series (Daily)'][day_b4_yesterday]['4. close'])
 
 per_change = (yesterdays_close - day_b4_yesterday_close)/yesterdays_close
 
-if per_change > .05: 
-    print("Get News")
-
-print(yesterdays_close)
-print(day_b4_yesterday_close)
 ## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+url_1 = (f'https://newsapi.org/v2/everything?'
+       'q=Tesla&'
+       'from=2025-04-17&'
+       'sortBy=popularity&'
+       'apiKey=')
 
+response = requests.get(url_1)
+news = response.json()
+
+if per_change >= .05 or per_change <= -.05: 
+    for a in range(3):
+        print(news['articles'][a])
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
 
